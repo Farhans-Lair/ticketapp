82a8fb919b6c750786ad90326a0e132000e60ba5
Farhan <farhanpoint2000@gmail.com>
2026-09-06 23:05:32 +0530

    update

--- terraform/asg.tf
+++ terraform/asg.tf
@@ -8,7 +8,6 @@ resource "aws_autoscaling_group" "backend_asg" {
   max_size         = var.asg_max_size
 
   vpc_zone_identifier = [
-    aws_subnet.private_subnet_1.id,
     aws_subnet.private_subnet_2.id
   ]
 
